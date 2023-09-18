d2a7762081501d71adfc8b7f7e410b0e8122ec54
Ahmed Shahzad <ahmedscriptx@gmail.com>
2023-09-18 10:26:16 +0100

    Create RandomChallenges.py

---
+++ RandomChallenges.py
@@ -0,0 +1,16 @@
+def areaOfATriangle(x,y):
+    return x*y/2
+
+xInput = int(input("Enter the legnth: "))
+yInput = int(input("Enter the width: "))
+
+print(areaOfATriangle(xInput,yInput))
+
+voltage = int(input("Enter the legnth: "))
+current = int(input("Enter the width: "))
+
+def calculatePower(x,y):
+    return x*y
+
+print(calculatePower(voltage,current))
+print("test2")
